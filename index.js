1208756a1ce0e6220f222213ae0c4073bb57b1d6
adnanmahmud0 <adnan.mahmud.pc@gmail.com>
2025-01-15 17:51:11 +0600

    user colledtion api added

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 //*******************************************************************************
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@adnan.f8a3c.mongodb.net/?retryWrites=true&w=majority&appName=adnan`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,14 +28,36 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const userCollection = client.db("edurock").collection("user");
+
     const courses = client.db("edurock").collection("classes");
+
+    const teacher = client.db("edurock").collection("teacher");
+
+    app.post('/user', async (req, res) => {
+        const user = req.body;
+        const result = await userCollection.insertOne(user);
+        res.send(result);
+    })
 
     app.get('/classes', async (req, res) => {
         const result = await courses.find().toArray();
         res.send(result);
     });
 
-    app.get('/class')
+    app.get('/class/:id', async (req, res) => {
+
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await courses.findOne(query);
+        res.send(result);
+    });
+
+    app.post('/teacher', async (req, res) => {
+        const item = req.body;
+        const result = await teacher.insertOne(item);
+        res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
