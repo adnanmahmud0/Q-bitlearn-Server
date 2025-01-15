79424e358356361ead8a1919feaab04eb5ad751f
adnanmahmud0 <adnan.mahmud.pc@gmail.com>
2025-01-15 11:37:10 +0600

    some updte done

--- index.js
+++ index.js
@@ -35,6 +35,8 @@ async function run() {
         res.send(result);
     });
 
+    app.get('/class')
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
